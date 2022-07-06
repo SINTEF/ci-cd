afd5b0065cdbb299abc4bc362bcb79d62024035d
Casper Welzel Andersen <casper.w.andersen@sintef.no>
2022-07-06 23:55:07 +0200

    Fix pyproject.toml console scripts

--- ci_cd/__init__.py
+++ ci_cd/__init__.py
@@ -1,4 +1,4 @@
-"""Tiny Python package to run invoke tasks as a standalone program."""
+"""CI/CD Tools. Tiny package to run invoke tasks as a standalone program."""
 __version__ = "1.1.0"
 __author__ = "Casper Welzel Andersen"
 __author_email__ = "casper+github@welzel.nu"
