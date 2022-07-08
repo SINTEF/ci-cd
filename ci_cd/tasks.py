17f66d036ef551a28f08c74bf336e1993fa5907b
Casper Welzel Andersen <casper.w.andersen@sintef.no>
2022-07-08 12:26:02 +0200

    New custom class for SemVer representation
    Test all new list of changes to perform when updating version

--- ci_cd/tasks.py
+++ ci_cd/tasks.py
@@ -11,7 +11,7 @@ from typing import TYPE_CHECKING
 from invoke import task
 
 if TYPE_CHECKING:  # pragma: no cover
-    from typing import Tuple
+    from typing import Optional, Tuple, Union
 
     from invoke import Context, Result
 
@@ -23,6 +23,105 @@ class Emoji(str, Enum):
     CHECK_MARK = "\u2714"
     CROSS_MARK = "\u274c"
     CURLY_LOOP = "\u27b0"
+
+
+class SemanticVersion:
+    """A semantic version.
+
+    See [SemVer.org](https://semver.org) for more information about semantic
+    versioning.
+
+    The semantic version is in this invocation considered to build up in the following
+    way:
+
+        <major>.<minor>.<patch>-<pre_release>+<build>
+
+    Where the names in carets are callable attributes for the instance.
+
+    When casting instances of `SemanticVersion` to `str`, the full version will be
+    returned, i.e., as shown above, with a minimum of major.minor.patch.
+
+    For example, for the version `1.5`, i.e., `major=1, minor=5`, the returned `str`
+    representation will be the full major.minor.patch version: `1.5.0`.
+    The `patch` attribute will default to `0` while `pre_release` and `build` will be
+    `None`, when asked for explicitly.
+
+    Parameters:
+        major (Union[str, int]): The major version.
+        minor (Optional[Union[str, int]]): The minor version.
+        patch (Optional[Union[str, int]]): The patch version.
+        pre_release (Optional[str]): The pre-release part of the version, i.e., the
+            part supplied after a minus (`-`), but before a plus (`+`).
+        build (Optional[str]): The build metadata part of the version, i.e., the part
+            supplied at the end of the version, after a plus (`+`).
+
+    Attributes:
+        major (int): The major version.
+        minor (int): The minor version.
+        patch (int): The patch version.
+        pre_release (str): The pre-release part of the version, i.e., the part
+            supplied after a minus (`-`), but before a plus (`+`).
+        build (str): The build metadata part of the version, i.e., the part supplied at
+            the end of the version, after a plus (`+`).
+
+    """
+
+    def __init__(
+        self,
+        major: "Union[str, int]",
+        minor: "Optional[Union[str, int]]" = None,
+        patch: "Optional[Union[str, int]]" = None,
+        pre_release: "Optional[str]" = None,
+        build: "Optional[str]" = None,
+    ) -> None:
+        self._major = int(major)
+        self._minor = int(minor) if minor else 0
+        self._patch = int(patch) if patch else 0
+        self._pre_release = pre_release if pre_release else None
+        self._build = build if build else None
+
+    @property
+    def major(self) -> int:
+        """The major version."""
+        return self._major
+
+    @property
+    def minor(self) -> int:
+        """The minor version."""
+        return self._minor
+
+    @property
+    def patch(self) -> int:
+        """The patch version."""
+        return self._patch
+
+    @property
+    def pre_release(self) -> "Union[None, str]":
+        """The pre-release part of the version
+
+        This is the part supplied after a minus (`-`), but before a plus (`+`).
+        """
+        return self._pre_release
+
+    @property
+    def build(self) -> "Union[None, str]":
+        """The build metadata part of the version.
+
+        This is the part supplied at the end of the version, after a plus (`+`).
+        """
+        return self._build
+
+    def __str__(self) -> str:
+        """Return the full version."""
+        return (
+            f"{self.major}.{self.minor}.{self.patch}"
+            f"{f'-{self.pre_release}' if self.pre_release else ''}"
+            f"{f'+{self.build}' if self.build else ''}"
+        )
+
+    def __repr__(self) -> str:
+        """Return the string representation of the object."""
+        return repr(self.__str__())
 
 
 def update_file(filename: Path, sub_line: "Tuple[str, str]", strip: str = None) -> None:
@@ -65,7 +164,7 @@ def update_file(filename: Path, sub_line: "Tuple[str, str]", strip: str = None) 
     },
     iterable=["code_base_update"],
 )
-def setver(
+def setver(  # pylint: disable=too-many-locals
     _,
     package_dir,
     version,
@@ -87,9 +186,9 @@ def setver(
 
     match = re.fullmatch(
         (
-            r"v?(?P<version>[0-9]+(\.[0-9]+){2}"  # Major.Minor.Patch
-            r"(-[0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*)?"  # pre-release
-            r"(\+[0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*)?)"  # build metadata
+            r"v?(?P<major>[0-9]+)\.(?P<minor>[0-9]+)\.(?P<patch>[0-9]+)"
+            r"(?:-(?P<pre_release>[0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?"
+            r"(?:\+(?P<build>[0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?"
         ),
         version,
     )
@@ -99,7 +198,7 @@ def setver(
             "'Major.Minor.Patch(-Pre-Release+Build Metadata)' or "
             "'vMajor.Minor.Patch(-Pre-Release+Build Metadata)'"
         )
-    version = match.group("version")
+    semantic_version = SemanticVersion(**match.groupdict())
 
     if not code_base_update:
         init_file = Path(root_repo_path).resolve() / package_dir / "__init__.py"
@@ -111,7 +210,10 @@ def setver(
 
         update_file(
             init_file,
-            (r'__version__ *= *(?:\'|").*(?:\'|")', f'__version__ = "{version}"'),
+            (
+                r'__version__ *= *(?:\'|").*(?:\'|")',
+                f'__version__ = "{semantic_version}"',
+            ),
         )
     else:
         errors: list[str] = []
@@ -130,7 +232,9 @@ def setver(
                 )
 
             filepath = Path(
-                filepath.format(**{"package_dir": package_dir, "version": version})
+                filepath.format(
+                    **{"package_dir": package_dir, "version": semantic_version}
+                )
             ).resolve()
             if not filepath.exists():
                 error_msg = (
@@ -148,7 +252,7 @@ def setver(
                 print(f"replacement (input): {replacement}")
                 print(
                     "replacement (handled): "
-                    f"{replacement.format(**{'package_dir': package_dir, 'version': version})}"  # pylint: disable=line-too-long
+                    f"{replacement.format(**{'package_dir': package_dir, 'version': semantic_version})}"  # pylint: disable=line-too-long
                 )
 
             update_file(
@@ -156,12 +260,15 @@ def setver(
                 (
                     pattern,
                     replacement.format(
-                        **{"package_dir": package_dir, "version": version}
+                        **{"package_dir": package_dir, "version": semantic_version}
                     ),
                 ),
             )
 
-    print(f"{Emoji.PARTY_POPPER.value} Bumped version for {package_dir} to {version}.")
+    print(
+        f"{Emoji.PARTY_POPPER.value} Bumped version for {package_dir} to "
+        f"{semantic_version}."
+    )
 
 
 @task(
